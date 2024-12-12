90084930f7486695db2b041cd09a7b0daf5998c1
tcabrag02 <tcabrag02@suarezdefigueroa.es>
2024-12-12 14:00:20 +0100

    subimos php

---
+++ php/php seccion 1/4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>¿Qué significa? ?php?</h1>
+<?php
+    echo "PHP es un lenguaje de programación de código abierto"
+?>
+<h1>¿Para qué sirve echo?</h1>
+<?php
+    echo"se utiliza para imprimir o mostrar mensajes o variables en la página web"
+?>
+<h1>¿Qué ocurre cuando ejecutas el archivo en un servidor?</h1>
+<?php
+    echo"Que se ejecuta la linea de comando"
+?>
+</body>
+</html>
---
+++ php/php seccion 1/index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/php" href="index.php">
+</head>
+<body>
+    <h2>1. Ve al tema y lee la sección "¿Qué es PHP?" en el documento.</h2>
+    <?php 
+    echo "PHP es un lenguaje de programación denominado “procesador de hipertexto”.
+        Este lenguaje se incrusta directamente dentro del código HTML. "
+    ?>
+    <h2>2.Con base en la lectura, responde las siguientes preguntas:</h2>
+    <h3>¿Dónde se ejecuta el código PHP: en el servidor o en el cliente?</h3>
+    <?php
+        PHP se ejecuta en el
+        servidor y es este el que envía el resultado de la ejecución al navegador para que
+        este lo interprete y lo muestre.
+    ?>
+    
+    <h3>¿Qué tipo de contenido puede generar PHP?</h3>
+    <?php
+        generar conteni-dos dinámicos (acceso a bases de datos)
+    ?>
+    <h3>¿Cuál es la diferencia entre PHP y JavaScript en términos de dónde se ejecutan?</h3>
+    <?php
+        La diferencia con
+        lenguajes como JavaScript es que el código PHP se ejecuta en el
+        servidor y es este el que envía el resultado de la ejecución al navegador para que
+        este lo interprete y lo muestre
+    ?>
+    
+</body>
+</html>
